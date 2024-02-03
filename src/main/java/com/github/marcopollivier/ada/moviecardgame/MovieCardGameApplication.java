9a1bb837f52b14325b529ea21dcd7505fa7405ee
Marco Ollivier <marcopollivier@gmail.com>
2024-02-03 11:09:33 -0300

    spring initializr

---
+++ src/main/java/com/github/marcopollivier/ada/moviecardgame/MovieCardGameApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcopollivier.ada.moviecardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCardGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCardGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/marcopollivier/ada/moviecardgame/MovieCardGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcopollivier.ada.moviecardgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCardGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
